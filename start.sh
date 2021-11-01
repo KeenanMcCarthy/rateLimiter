ce2034d986d43f48daca1159c9a00cc62cfbc5f9
KeenanMcCarthy <85964129+KeenanMcCarthy@users.noreply.github.com>
2021-11-01 15:08:35 -0400

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. ${PWD}/config
+
+#start loadBalancer
+PORT=$LBPORT DESTIP=$DESTIP DESTPORT=$DESTPORT QSIZE=$QUORUMSIZE LIMIT=$MAXREQ node loadBalancer.js &> logs.txt &
+
+#loop through and start servers
+for i in "${SERVERPORTS[@]}"
+do
+  PGPASSWORD=$PGPSSWRD psql -U $PGUNAME -c "CREATE DATABASE rlproject${i};"
+  PGPASSWORD=$PGPSSWRD psql -U $PGUNAME -d rlproject${i} -f rateLimitingDB.sql 
+  PORT=$i LB=$LBPORT TINT=$TIMEINT MAXREQ=$MAXREQ node server.js &> logs.txt &
+done
